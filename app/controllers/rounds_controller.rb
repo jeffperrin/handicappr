825368e45116bb651dec6b267230b49ed0594756
Jeff Perrin <jeff@jeffperrin.com>
2009-10-17 07:20:51 -0600

    Added editing of rounds, made a bunch of style/layout tweaks

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -2,6 +2,24 @@ class RoundsController < ApplicationController
   
   def index
     fetch_user_by_name
+    @rounds = Round.paginate_by_user_id @user.id, :page => params[:page], :order => 'updated_at DESC', :per_page => 10
+  end
+  
+  def edit
+    @round = Round.find(params[:id])
+  end
+  
+  def update
+    @round = Round.find(params[:id])
+    respond_to do |f|
+      if @round.update_attributes(params[:round])
+        flash[:notice] = 'Round has been updated'
+        f.html {redirect_to history_path :user => current_user.username}
+      else
+        flash[:error] = 'Couldn\'t edit the round'
+        f.html {render :template => 'rounds/edit'}
+      end
+    end
   end
   
   def create
